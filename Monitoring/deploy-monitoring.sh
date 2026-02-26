9b5d2e0b9b3911c994f0ebe15198780f022ba0b5
VenkeyBoda <venkeyboda07@gmail.com>
2026-02-26 17:36:46 +0530

    added promothoes url in monitoring script

--- Monitoring/deploy-monitoring.sh
+++ Monitoring/deploy-monitoring.sh
@@ -55,11 +55,50 @@ echo "========================================="
 GRAFANA_PASSWORD=$(kubectl get secret -n monitoring monitoring-grafana \
   -o jsonpath="{.data.admin-password}" | base64 --decode)
 
+# -------------------- Prometheus --------------------
+echo "========================================="
+echo " Waiting for Prometheus LoadBalancer"
+echo "========================================="
+
+PROM_ATTEMPTS=30
+PROM_SLEEP_SECONDS=10
+
+for ((i=1;i<=PROM_ATTEMPTS;i++)); do
+
+  PROM_IP=$(kubectl get svc monitoring-kube-prometheus-prometheus -n monitoring -o jsonpath='{.status.loadBalancer.ingress[0].ip}' 2>/dev/null)
+  PROM_HOSTNAME=$(kubectl get svc monitoring-kube-prometheus-prometheus -n monitoring -o jsonpath='{.status.loadBalancer.ingress[0].hostname}' 2>/dev/null)
+
+  if [ -n "$PROM_IP" ]; then
+      echo "Prometheus IP assigned!"
+      PROM_URL=$PROM_IP
+      break
+  fi
+
+  if [ -n "$PROM_HOSTNAME" ]; then
+      echo "Prometheus Hostname assigned!"
+      PROM_URL=$PROM_HOSTNAME
+      break
+  fi
+
+  echo "Waiting... attempt $i/$PROM_ATTEMPTS"
+  sleep $PROM_SLEEP_SECONDS
+done
+
+if [ -z "$PROM_URL" ]; then
+  echo "ERROR: Prometheus LoadBalancer not ready."
+  exit 1
+fi  
+
+# -------------------- Summary --------------------
 echo ""
 echo "========================================="
-echo " Grafana Access Details"
+echo " Monitoring Stack Access Details"
 echo "========================================="
+echo "Grafana:"
 echo "URL: http://$GRAFANA_URL"
 echo "Username: admin"
 echo "Password: $GRAFANA_PASSWORD"
+echo "-----------------------------------------"
+echo "Prometheus:"
+echo "URL: http://$PROM_URL:9090"
 echo "========================================="
